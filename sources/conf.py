fdbc2425333928fbfe59a9e9ba017bff06bbfc2b
luminous <luminaries@riseup.now>
2014-05-13 11:47:47 +0000

    t2432: update conf.py to use oms-doc-themes/oms

--- sources/conf.py
+++ sources/conf.py
@@ -122,7 +122,7 @@ html_theme = 'oms'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = ['../theme']
+html_theme_path = ['../../oms-doc-themes/']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
