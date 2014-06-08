dcda06aa1c2c25cba4d22480586f4fbed5f4ab8b
luminous <luminaries@riseup.now>
2014-06-08 04:35:09 +0000

    t2455: align the great Sphinx
    * correct license in sources/conf.py

--- sources/conf.py
+++ sources/conf.py
@@ -1,7 +1,37 @@
 # -*- coding: utf-8 -*-
 #
-# Open Mustard Seed documentation build configuration file, created by
-# sphinx-quickstart on Tue Mar 19 12:34:07 2013.
+# Copyright (C) 2014 the Institute for Institutional Innovation by Data
+# Driven Design Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE INSTITUTE FOR INSTITUTIONAL
+# INNOVATION BY DATA DRIVEN DESIGN INC. BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+# OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Except as contained in this notice, the names of the Institute for
+# Institutional Innovation by Data Driven Design Inc. shall not be used in
+# advertising or otherwise to promote the sale, use or other dealings
+# in this Software without prior written authorization from the
+# Institute for Institutional Innovation by Data Driven Design Inc.
+#
+
+import sys, os
+
 #
 # This file is execfile()d with the current directory set to its containing dir.
 #
@@ -10,8 +40,6 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
-import sys, os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
